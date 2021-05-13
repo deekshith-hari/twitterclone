7cbeb2c11e9fed2a1932dd1cbdfa05a84251ad9f
deekshith-hari <deekshith@musubi-management.com>
2021-05-14 01:49:25 +0530

    requirements.txt

--- twitterclone/settings.py
+++ twitterclone/settings.py
@@ -124,8 +124,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATIC_URL = '/static/'
+
 STATICFILES_STORAGE = whitenoise.storage.CompressedMainfestStaticFilesStorage
 
 MEDIA_URL = '/twitterclone/'
