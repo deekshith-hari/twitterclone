844ea09daa6e5e364cf1919aa3cccea66c9e6886
deekshith-hari <deekshith@musubi-management.com>
2021-05-14 01:56:31 +0530

    requirements.txt

--- twitterclone/settings.py
+++ twitterclone/settings.py
@@ -129,7 +129,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
-STATICFILES_STORAGE = whitenoise.storage.CompressedMainfestStaticFilesStorage
 
 MEDIA_URL = '/twitterclone/'
 
